53cb8e700cf164eb45d657a964906dc36f2a6b66
xiaowei87 <66402453+xiaowei87@users.noreply.github.com>
2020-06-04 16:17:25 +0800

    1st Release
    Added Visual C# project and source code.

---
+++ Visual_CS/CSV_To_I2C_Log/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace CSV_To_I2C_Log
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BtnConvert = new System.Windows.Forms.Button();
+            this.BtnAbout = new System.Windows.Forms.Button();
+            this.ChkTimestamp = new System.Windows.Forms.CheckBox();
+            this.LblSCL = new System.Windows.Forms.Label();
+            this.Rbtn250 = new System.Windows.Forms.RadioButton();
+            this.GrpSamplingRate = new System.Windows.Forms.GroupBox();
+            this.Rbtn1000 = new System.Windows.Forms.RadioButton();
+            this.Rbtn500 = new System.Windows.Forms.RadioButton();
+            this.NumSCL = new System.Windows.Forms.NumericUpDown();
+            this.LblSDA = new System.Windows.Forms.Label();
+            this.LblColumn = new System.Windows.Forms.Label();
+            this.NumSDA = new System.Windows.Forms.NumericUpDown();
+            this.LblDelimiter = new System.Windows.Forms.Label();
+            this.TxtDelimiter = new System.Windows.Forms.TextBox();
+            this.GrpSamplingRate.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.NumSCL)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumSDA)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnConvert
+            // 
+            this.BtnConvert.Location = new System.Drawing.Point(122, 78);
+            this.BtnConvert.Name = "BtnConvert";
+            this.BtnConvert.Size = new System.Drawing.Size(57, 40);
+            this.BtnConvert.TabIndex = 0;
+            this.BtnConvert.Text = "Convert";
+            this.BtnConvert.UseVisualStyleBackColor = true;
+            this.BtnConvert.Click += new System.EventHandler(this.BtnConvert_Click);
+            // 
+            // BtnAbout
+            // 
+            this.BtnAbout.Location = new System.Drawing.Point(122, 124);
+            this.BtnAbout.Name = "BtnAbout";
+            this.BtnAbout.Size = new System.Drawing.Size(57, 40);
+            this.BtnAbout.TabIndex = 1;
+            this.BtnAbout.Text = "About";
+            this.BtnAbout.UseVisualStyleBackColor = true;
+            // 
+            // ChkTimestamp
+            // 
+            this.ChkTimestamp.AutoSize = true;
+            this.ChkTimestamp.Checked = true;
+            this.ChkTimestamp.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ChkTimestamp.Location = new System.Drawing.Point(12, 51);
+            this.ChkTimestamp.Name = "ChkTimestamp";
+            this.ChkTimestamp.Size = new System.Drawing.Size(77, 17);
+            this.ChkTimestamp.TabIndex = 2;
+            this.ChkTimestamp.Text = "Timestamp";
+            this.ChkTimestamp.UseVisualStyleBackColor = true;
+            this.ChkTimestamp.CheckedChanged += new System.EventHandler(this.ChkTimestamp_CheckedChanged);
+            // 
+            // LblSCL
+            // 
+            this.LblSCL.AutoSize = true;
+            this.LblSCL.Location = new System.Drawing.Point(65, 9);
+            this.LblSCL.Name = "LblSCL";
+            this.LblSCL.Size = new System.Drawing.Size(27, 13);
+            this.LblSCL.TabIndex = 3;
+            this.LblSCL.Text = "SCL";
+            // 
+            // Rbtn250
+            // 
+            this.Rbtn250.AutoSize = true;
+            this.Rbtn250.Checked = true;
+            this.Rbtn250.Location = new System.Drawing.Point(6, 19);
+            this.Rbtn250.Name = "Rbtn250";
+            this.Rbtn250.Size = new System.Drawing.Size(63, 17);
+            this.Rbtn250.TabIndex = 4;
+            this.Rbtn250.TabStop = true;
+            this.Rbtn250.Text = "250KHz";
+            this.Rbtn250.UseVisualStyleBackColor = true;
+            // 
+            // GrpSamplingRate
+            // 
+            this.GrpSamplingRate.Controls.Add(this.Rbtn1000);
+            this.GrpSamplingRate.Controls.Add(this.Rbtn500);
+            this.GrpSamplingRate.Controls.Add(this.Rbtn250);
+            this.GrpSamplingRate.Location = new System.Drawing.Point(12, 74);
+            this.GrpSamplingRate.Name = "GrpSamplingRate";
+            this.GrpSamplingRate.Size = new System.Drawing.Size(104, 93);
+            this.GrpSamplingRate.TabIndex = 5;
+            this.GrpSamplingRate.TabStop = false;
+            this.GrpSamplingRate.Text = "Sampling Rate";
+            // 
+            // Rbtn1000
+            // 
+            this.Rbtn1000.AutoSize = true;
+            this.Rbtn1000.Location = new System.Drawing.Point(6, 65);
+            this.Rbtn1000.Name = "Rbtn1000";
+            this.Rbtn1000.Size = new System.Drawing.Size(53, 17);
+            this.Rbtn1000.TabIndex = 6;
+            this.Rbtn1000.Text = "1MHz";
+            this.Rbtn1000.UseVisualStyleBackColor = true;
+            // 
+            // Rbtn500
+            // 
+            this.Rbtn500.AutoSize = true;
+            this.Rbtn500.Location = new System.Drawing.Point(6, 42);
+            this.Rbtn500.Name = "Rbtn500";
+            this.Rbtn500.Size = new System.Drawing.Size(63, 17);
+            this.Rbtn500.TabIndex = 5;
+            this.Rbtn500.Text = "500KHz";
+            this.Rbtn500.UseVisualStyleBackColor = true;
+            // 
+            // NumSCL
+            // 
+            this.NumSCL.Location = new System.Drawing.Point(68, 25);
+            this.NumSCL.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.NumSCL.Name = "NumSCL";
+            this.NumSCL.Size = new System.Drawing.Size(50, 20);
+            this.NumSCL.TabIndex = 6;
+            this.NumSCL.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // LblSDA
+            // 
+            this.LblSDA.AutoSize = true;
+            this.LblSDA.Location = new System.Drawing.Point(126, 9);
+            this.LblSDA.Name = "LblSDA";
+            this.LblSDA.Size = new System.Drawing.Size(29, 13);
+            this.LblSDA.TabIndex = 7;
+            this.LblSDA.Text = "SDA";
+            // 
+            // LblColumn
+            // 
+            this.LblColumn.AutoSize = true;
+            this.LblColumn.Location = new System.Drawing.Point(17, 27);
+            this.LblColumn.Name = "LblColumn";
+            this.LblColumn.Size = new System.Drawing.Size(45, 13);
+            this.LblColumn.TabIndex = 8;
+            this.LblColumn.Text = "Column:";
+            // 
+            // NumSDA
+            // 
+            this.NumSDA.Location = new System.Drawing.Point(129, 25);
+            this.NumSDA.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.NumSDA.Name = "NumSDA";
+            this.NumSDA.Size = new System.Drawing.Size(50, 20);
+            this.NumSDA.TabIndex = 9;
+            this.NumSDA.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // LblDelimiter
+            // 
+            this.LblDelimiter.AutoSize = true;
+            this.LblDelimiter.Location = new System.Drawing.Point(95, 52);
+            this.LblDelimiter.Name = "LblDelimiter";
+            this.LblDelimiter.Size = new System.Drawing.Size(50, 13);
+            this.LblDelimiter.TabIndex = 10;
+            this.LblDelimiter.Text = "Delimiter:";
+            // 
+            // TxtDelimiter
+            // 
+            this.TxtDelimiter.Location = new System.Drawing.Point(151, 49);
+            this.TxtDelimiter.MaxLength = 1;
+            this.TxtDelimiter.Name = "TxtDelimiter";
+            this.TxtDelimiter.Size = new System.Drawing.Size(28, 20);
+            this.TxtDelimiter.TabIndex = 11;
+            this.TxtDelimiter.Text = ",";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(189, 176);
+            this.Controls.Add(this.TxtDelimiter);
+            this.Controls.Add(this.LblDelimiter);
+            this.Controls.Add(this.NumSDA);
+            this.Controls.Add(this.LblColumn);
+            this.Controls.Add(this.LblSDA);
+            this.Controls.Add(this.NumSCL);
+            this.Controls.Add(this.GrpSamplingRate);
+            this.Controls.Add(this.LblSCL);
+            this.Controls.Add(this.ChkTimestamp);
+            this.Controls.Add(this.BtnAbout);
+            this.Controls.Add(this.BtnConvert);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "CSV To I2C Log Converter";
+            this.GrpSamplingRate.ResumeLayout(false);
+            this.GrpSamplingRate.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.NumSCL)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumSDA)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnConvert;
+        private System.Windows.Forms.Button BtnAbout;
+        private System.Windows.Forms.CheckBox ChkTimestamp;
+        private System.Windows.Forms.Label LblSCL;
+        private System.Windows.Forms.RadioButton Rbtn250;
+        private System.Windows.Forms.GroupBox GrpSamplingRate;
+        private System.Windows.Forms.RadioButton Rbtn1000;
+        private System.Windows.Forms.RadioButton Rbtn500;
+        private System.Windows.Forms.Label LblSDA;
+        private System.Windows.Forms.Label LblColumn;
+        private System.Windows.Forms.NumericUpDown NumSDA;
+        private System.Windows.Forms.Label LblDelimiter;
+        private System.Windows.Forms.TextBox TxtDelimiter;
+        private System.Windows.Forms.NumericUpDown NumSCL;
+    }
+}
+
---
+++ Visual_CS/CSV_To_I2C_Log/Form1.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSV_To_I2C_Log
+{
+    public partial class Form1 : Form
+    {
+        enum I2C_Status
+        {
+            Idle,
+            StartDetected,
+            WaitSCLHigh,
+            WaitSCLLow,
+            Error
+        }
+
+        List<string> listSCL = new List<string>();
+        List<string> listSDA = new List<string>();
+
+        string csvpath = "";
+        string txtpath = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnConvert_Click(object sender, EventArgs e)
+        {
+            bool filecheck;
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = "D:\\";
+            openFileDialog1.Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                csvpath = openFileDialog1.FileName;
+            }
+
+            filecheck = File.Exists(csvpath);
+
+            if (filecheck)
+            {
+                txtpath = Path.ChangeExtension(csvpath, "log");
+
+                if (File.Exists(txtpath))
+                {
+                    DialogResult dialogResult = MessageBox.Show(txtpath + " exists, do you want to replace?", "Warning", MessageBoxButtons.YesNo);
+                    if (dialogResult == DialogResult.Yes)
+                    {
+                        using (StreamWriter sw = File.CreateText(txtpath))
+                        {
+                            sw.WriteLine("");
+                        }
+                        filecheck = true;
+                    }
+                    else //if (dialogResult == DialogResult.No)
+                    {
+                        filecheck = false;
+                        MessageBox.Show("Conversion cancelled.", "Info", MessageBoxButtons.OK);
+                    }
+                }
+                else
+                {
+                    using (StreamWriter sw = File.CreateText(txtpath))
+                    {
+                        sw.WriteLine("");
+                    }
+                    filecheck = true;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Selected CSV file is invalid.", "Error", MessageBoxButtons.OK);
+            }
+
+            if (filecheck)
+            {
+                if (NumSCL.Value != NumSDA.Value)
+                {
+                    CSV_To_I2C_Log();
+                }
+                else
+                {
+                    MessageBox.Show("Column of SCL cannot be same as Column of SDA.", "Error", MessageBoxButtons.OK);
+                }
+            }
+        }
+
+        private void CSV_To_I2C_Log()
+        {
+            int ColSCL = Decimal.ToInt16(NumSCL.Value);
+            int ColSDA = Decimal.ToInt16(NumSDA.Value);
+
+            using (var sr = new StreamReader(csvpath))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine();
+                    var values = line.Split(',');
+
+                    if (values.Length < ColSCL)
+                    {
+                        MessageBox.Show("SCL data stopped unexpectedly @ " + listSCL.Count, "Info", MessageBoxButtons.OK);
+                        break;
+                    }
+                    else if (values.Length < ColSDA)
+                    {
+                        MessageBox.Show("SDA data stopped unexpectedly @ " + listSDA.Count, "Info", MessageBoxButtons.OK);
+                        break;
+                    }
+                    else
+                    {
+                        listSCL.Add(values[(ColSCL - 1)]);
+                        listSDA.Add(values[(ColSDA - 1)]);
+                    }
+                }
+
+                if ((listSCL.Count > 1) && (listSDA.Count > 1))
+                {
+                    I2C_Status i2cStatus = I2C_Status.Idle;
+                    int i2cBitPtr = 0;
+                    int i2cData = 0;
+                    string preSDA = "0";
+                    bool timestampcheck = ChkTimestamp.Enabled;
+                    double timestamp = 0;
+
+                    using (StreamWriter sw = File.AppendText(txtpath))
+                    {
+                        for (int i = 1; i < listSCL.Count; i++)
+                        {
+                            switch (i2cStatus)
+                            {
+                                case I2C_Status.Idle:
+                                    if ((listSCL[i] == "1") && (listSDA[i]) == "1")
+                                    {
+                                        // Idle
+                                    }
+                                    else if ((listSCL[i] == "1") && (listSDA[i]) == "0")
+                                    {
+                                        // Start Bit
+                                        i2cStatus = I2C_Status.StartDetected;
+                                        
+                                        if (timestampcheck)
+                                        {
+                                            timestamp = 0;
+                                            if (Rbtn250.Checked)
+                                            {
+                                                timestamp = i * 3.56 / 1000;
+                                            }
+                                            else if (Rbtn500.Checked)
+                                            {
+                                                timestamp = i * 1.78 / 1000;
+                                            }
+                                            else if (Rbtn1000.Checked)
+                                            {
+                                                timestamp = i * 0.89 / 1000;
+                                            }
+                                            sw.Write(timestamp.ToString("0.00") + "ms:");
+                                        }
+                                        sw.Write("[S] ");
+                                    }
+                                    else if ((listSCL[i] == "0") && (listSDA[i]) == "1")
+                                    {
+                                        // Error
+                                        i2cStatus = I2C_Status.Error;
+                                    }
+                                    else if ((listSCL[i] == "0") && (listSDA[i]) == "0")
+                                    {
+                                        // Error
+                                        i2cStatus = I2C_Status.Error;
+                                    }
+                                    break;
+                                case I2C_Status.StartDetected:
+                                    if ((listSCL[i] == "0") && (listSDA[i]) == "0")
+                                    {
+                                        // Ready to wait for data
+                                        i2cStatus = I2C_Status.WaitSCLHigh;
+                                        i2cBitPtr = 0;
+                                        i2cData = 0;
+                                    }
+                                    break;
+                                case I2C_Status.WaitSCLHigh:
+                                    if (listSCL[i] == "1")
+                                    {
+                                        // SCL High, read bit
+                                        if (i2cBitPtr < 8) // Bit0-7 Data
+                                        {
+                                            if (listSDA[i] == "1")
+                                            {
+                                                i2cData |= (1 << (7 - i2cBitPtr));
+                                            }
+                                            else
+                                            {
+
+                                            }
+                                        }
+                                        else // Bit8 ACK
+                                        {
+                                            sw.Write(i2cData.ToString("X2"));
+
+                                            if (listSDA[i] == "0")
+                                            {
+                                                sw.Write(" ");
+                                            }
+                                            else
+                                            {
+                                                sw.Write("?");
+                                            }
+                                            i2cData = 0;
+                                        }
+                                        i2cStatus = I2C_Status.WaitSCLLow;
+                                        i2cBitPtr++;
+                                        preSDA = listSDA[i];
+                                    }
+                                    break;
+                                case I2C_Status.WaitSCLLow:
+                                    if (listSCL[i] == "0")
+                                    {
+                                        i2cStatus = I2C_Status.WaitSCLHigh;
+
+                                        if (i2cBitPtr >= 9)
+                                        {
+                                            i2cBitPtr = 0;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        if (preSDA == "0")
+                                        {
+                                            if (preSDA != listSDA[i])
+                                            {
+                                                preSDA = listSDA[i];
+
+                                                if (listSDA[i] == "1")
+                                                {
+                                                    sw.WriteLine("[P]");
+                                                    i2cStatus = I2C_Status.Idle;
+                                                    i2cBitPtr = 0;
+                                                    i2cData = 0;
+                                                }
+                                            }
+                                        }
+                                        else if (preSDA == "1")
+                                        {
+                                            if (preSDA != listSDA[i])
+                                            {
+                                                preSDA = listSDA[i];
+
+                                                if (listSDA[i] == "0")
+                                                {
+                                                    sw.Write("[S]");
+                                                    i2cStatus = I2C_Status.StartDetected;
+                                                    i2cBitPtr = 0;
+                                                    i2cData = 0;
+                                                }
+                                            }
+                                        }
+                                    }
+                                    break;
+                                case I2C_Status.Error:
+                                default:
+                                    if ((listSCL[i] == "1") && (listSDA[i]) == "1")
+                                    {
+                                        // Back to Idle
+                                        i2cStatus = I2C_Status.Idle;
+                                    }
+                                    break;
+                            }
+                        }
+                        MessageBox.Show("Conversion is complete.", "Info", MessageBoxButtons.OK);
+                    }
+                }
+            }
+        }
+
+        private void ChkTimestamp_CheckedChanged(object sender, EventArgs e)
+        {
+#if false
+            if (ChkTimestamp.Checked)
+            {
+                GrpSamplingRate.Enabled = true;
+            }
+            else
+            {
+                GrpSamplingRate.Enabled = false;
+            }
+#else
+            GrpSamplingRate.Enabled = ChkTimestamp.Checked;
+#endif
+        }
+    }
+}
